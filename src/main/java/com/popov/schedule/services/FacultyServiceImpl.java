32233353d486e7cff1ebecaa00dcfb00b85fa3cc
anton <popovwork@gmail.com>
2022-01-20 17:53:59 +0200

    added functional to implementation faculty service

--- src/main/java/com/popov/schedule/services/FacultyServiceImpl.java
+++ src/main/java/com/popov/schedule/services/FacultyServiceImpl.java
@@ -1,7 +1,40 @@
 package com.popov.schedule.services;
 
+import com.popov.schedule.models.Faculty;
+import com.popov.schedule.repositories.FacultyRepository;
+import com.sun.xml.bind.v2.model.core.ID;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FacultyServiceImpl implements FacultyService {
+
+    private final FacultyRepository facultyRepository;
+
+    public FacultyServiceImpl(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
+
+
+    @Override
+    public void saveFaculty(Faculty faculty) {
+        facultyRepository.save(faculty);
+    }
+
+    @Override
+    public Optional<Faculty> getFacultyById(Long id) {
+        return facultyRepository.getFacultyById(id);
+    }
+
+    @Override
+    public Optional<Faculty> getFacultyByName(String name) {
+        return facultyRepository.getAllByName(name);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        facultyRepository.deleteById(id);
+    }
 }
