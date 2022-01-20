aa2faf832740986800e9ee8eba915b7a6f129ab2
anton <popovwork@gmail.com>
2022-01-20 17:53:44 +0200

    added functional to interface faculty service

--- src/main/java/com/popov/schedule/services/FacultyService.java
+++ src/main/java/com/popov/schedule/services/FacultyService.java
@@ -1,7 +1,22 @@
 package com.popov.schedule.services;
 
+import com.popov.schedule.models.Faculty;
+import com.popov.schedule.repositories.FacultyRepository;
+import com.sun.xml.bind.v2.model.core.ID;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public interface FacultyService {
+
+    void saveFaculty(Faculty faculty);
+
+    Optional<Faculty> getFacultyById(Long id);
+
+    Optional<Faculty> getFacultyByName(String name);
+
+    void deleteById(Long id);
+
 }
