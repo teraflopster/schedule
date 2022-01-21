7db6b3a4b0daffae838465c3f908a4199036d46b
anton <popovwork@gmail.com>
2022-01-21 17:33:13 +0200

    created model for table day

--- src/main/java/com/popov/schedule/models/Day.java
+++ src/main/java/com/popov/schedule/models/Day.java
@@ -1,5 +1,36 @@
 package com.popov.schedule.models;
 
+import lombok.Data;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Data
+@Entity
+@Table(name = "day")
 public class Day {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "date")
+    private LocalDate localDate;
+
+    @Column(name = "student_group_id")
+    private long studentGroupId;
+
+    public Day() {}
+
+    public Day(String date, long student_group_id) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern( "MM/dd/yyyy" );
+        this.localDate = LocalDate.parse(date, formatter);
+        this.studentGroupId = student_group_id;
+    }
+
+    public void setDate(String date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern( "MM/dd/yyyy" );
+        this.localDate = LocalDate.parse(date, formatter);
+    }
 }
