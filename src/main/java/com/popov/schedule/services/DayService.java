b2bc66298cd33cb5b85a6cc9443849aecdb04826
anton <popovwork@gmail.com>
2022-01-21 17:34:13 +0200

    created service and service implemented for table day

--- src/main/java/com/popov/schedule/services/DayService.java
+++ src/main/java/com/popov/schedule/services/DayService.java
@@ -1,7 +1,20 @@
 package com.popov.schedule.services;
 
+import com.popov.schedule.models.Day;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public interface DayService {
+    void saveDay(Day day);
+
+    Optional<Day> getDayById(Long id);
+
+    void deleteDayById(Long id);
+
+    List<Day> getAllDays();
+
+    Optional<Day> getDayByDateAndGroupId(String date, Long id);
 }
