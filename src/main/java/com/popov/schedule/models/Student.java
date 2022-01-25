4f04d2d9045c14c40866974166e7cad94536ed1d
anton <popovwork@gmail.com>
2022-01-25 09:58:32 +0200

    created student model

--- src/main/java/com/popov/schedule/models/Student.java
+++ src/main/java/com/popov/schedule/models/Student.java
@@ -1,4 +1,33 @@
 package com.popov.schedule.models;
 
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "student")
 public class Student {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "student_group_id")
+    private long studentGroupId;
+
+    public Student() {}
+
+    public Student(String name, long department_id) {
+        this.name = name;
+        this.studentGroupId = department_id;
+    }
 }
