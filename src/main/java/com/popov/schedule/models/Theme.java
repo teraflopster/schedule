82efb71bb650ab78472d96bb410d7635c22ec525
anton <popovwork@gmail.com>
2022-01-25 10:01:28 +0200

    created Theme model

--- src/main/java/com/popov/schedule/models/Theme.java
+++ src/main/java/com/popov/schedule/models/Theme.java
@@ -1,4 +1,33 @@
 package com.popov.schedule.models;
 
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "theme")
 public class Theme {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "teacher_id")
+    private long teacherId;
+
+    public Theme() {}
+
+    public Theme(String name, long teacher_id) {
+        this.name = name;
+        this.teacherId = teacher_id;
+    }
 }
