1baedd20d2732ca9283d61eaf11930c5030a4be5
anton <popovwork@gmail.com>
2022-01-25 09:54:18 +0200

    created day repository

--- src/main/java/com/popov/schedule/repositories/DayRepository.java
+++ src/main/java/com/popov/schedule/repositories/DayRepository.java
@@ -5,9 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DayRepository extends JpaRepository<Day, Long> {
-    Optional<Day> getDayByLocalDateAndStudentGroupId(LocalDate localDate, Long id);
 }
