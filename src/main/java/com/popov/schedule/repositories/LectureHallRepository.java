b75e230a2d0bc508e69f1fc2a7e976546fbc3663
anton <popovwork@gmail.com>
2022-01-25 09:57:46 +0200

    created LectureHall repository

--- src/main/java/com/popov/schedule/repositories/LectureHallRepository.java
+++ src/main/java/com/popov/schedule/repositories/LectureHallRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LectureHallRepository extends JpaRepository<LectureHall, ID> {
+public interface LectureHallRepository extends JpaRepository<LectureHall, Long> {
 }
