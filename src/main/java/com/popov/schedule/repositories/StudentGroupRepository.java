e216549b775b404d51bcd8adc726e36f9343525a
anton <popovwork@gmail.com>
2022-01-25 09:58:55 +0200

    created StudentGroup repository

--- src/main/java/com/popov/schedule/repositories/StudentGroupRepository.java
+++ src/main/java/com/popov/schedule/repositories/StudentGroupRepository.java
@@ -5,6 +5,11 @@ import com.sun.xml.bind.v2.model.core.ID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface StudentGroupRepository extends JpaRepository<StudentGroup, ID> {
+public interface StudentGroupRepository extends JpaRepository<StudentGroup, Long> {
+    Optional<StudentGroup> getStudentGroupById(Long id);
+    List<StudentGroup> getStudentGroupByCourseAndDepartmentId(Long course, Long departmentId);
 }
