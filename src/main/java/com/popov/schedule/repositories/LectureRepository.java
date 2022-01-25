5803dcf4b9a2b5813fe606d1cdc2584611b53a91
anton <popovwork@gmail.com>
2022-01-25 09:57:54 +0200

    created Lecture repository

--- src/main/java/com/popov/schedule/repositories/LectureRepository.java
+++ src/main/java/com/popov/schedule/repositories/LectureRepository.java
@@ -5,6 +5,9 @@ import com.sun.xml.bind.v2.model.core.ID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface LectureRepository extends JpaRepository<Lecture, ID> {
+public interface LectureRepository extends JpaRepository<Lecture, Long> {
+    List<Lecture> getLectureByDay(Long day_id);
 }
