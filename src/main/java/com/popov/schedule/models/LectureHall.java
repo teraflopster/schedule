2774f740896ff39d0552304cd8775a363a0e7ffd
anton <popovwork@gmail.com>
2022-01-25 09:57:17 +0200

    created LectureHall model

--- src/main/java/com/popov/schedule/models/LectureHall.java
+++ src/main/java/com/popov/schedule/models/LectureHall.java
@@ -1,4 +1,29 @@
 package com.popov.schedule.models;
 
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "lecture_hall")
 public class LectureHall {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public LectureHall() {}
+
+    public LectureHall(String name) {
+        this.name = name;
+    }
 }
