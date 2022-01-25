1092d6a4509a953ee08b5cd65cac60fb4db843b7
anton <popovwork@gmail.com>
2022-01-25 09:59:14 +0200

    implemented student service

--- src/main/java/com/popov/schedule/services/StudentGroupService.java
+++ src/main/java/com/popov/schedule/services/StudentGroupService.java
@@ -1,7 +1,20 @@
 package com.popov.schedule.services;
 
+import com.popov.schedule.models.StudentGroup;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public interface StudentGroupService {
+    void saveStudentGroup(StudentGroup studentGroup);
+
+    Optional<StudentGroup> getStudentGroupById(Long id);
+
+    void deleteStudentGroupById(Long id);
+
+    List<StudentGroup> getAllStudent();
+
+    List<StudentGroup> getGroupByCourseAndDepartment(Long course, Long department);
 }
--- src/main/java/com/popov/schedule/services/StudentGroupServiceImpl.java
+++ src/main/java/com/popov/schedule/services/StudentGroupServiceImpl.java
@@ -1,7 +1,43 @@
 package com.popov.schedule.services;
 
+import com.popov.schedule.models.StudentGroup;
+import com.popov.schedule.repositories.StudentGroupRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentGroupServiceImpl implements StudentGroupService {
+
+    private final StudentGroupRepository studentGroupRepository;
+
+    public StudentGroupServiceImpl(StudentGroupRepository studentGroupRepository) {
+        this.studentGroupRepository = studentGroupRepository;
+    }
+
+    @Override
+    public void saveStudentGroup(StudentGroup studentGroup) {
+        studentGroupRepository.save(studentGroup);
+    }
+
+    @Override
+    public Optional<StudentGroup> getStudentGroupById(Long id) {
+        return studentGroupRepository.getStudentGroupById(id);
+    }
+
+    @Override
+    public void deleteStudentGroupById(Long id) {
+        studentGroupRepository.deleteById(id);
+    }
+
+    @Override
+    public List<StudentGroup> getAllStudent() {
+        return studentGroupRepository.findAll();
+    }
+
+    @Override
+    public List<StudentGroup> getGroupByCourseAndDepartment(Long course, Long departmentId) {
+        return studentGroupRepository.getStudentGroupByCourseAndDepartmentId(course, departmentId);
+    }
 }
