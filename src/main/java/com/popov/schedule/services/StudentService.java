cc0eff5b2c30ba04a7126aec106e2281a03322ae
anton <popovwork@gmail.com>
2022-01-25 10:00:40 +0200

    implemented StudentService

--- src/main/java/com/popov/schedule/services/StudentService.java
+++ src/main/java/com/popov/schedule/services/StudentService.java
@@ -1,7 +1,13 @@
 package com.popov.schedule.services;
 
+import com.popov.schedule.models.Student;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public interface StudentService {
+    void saveStudent(Student student);
+
+    Optional<Student> getStudentById(Long id);
 }
--- src/main/java/com/popov/schedule/services/StudentServiceImpl.java
+++ src/main/java/com/popov/schedule/services/StudentServiceImpl.java
@@ -1,7 +1,27 @@
 package com.popov.schedule.services;
 
+import com.popov.schedule.models.Student;
+import com.popov.schedule.repositories.StudentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class StudentServiceImpl implements StudentService{
+
+    private final StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public void saveStudent(Student student) {
+        studentRepository.save(student);
+    }
+
+    @Override
+    public Optional<Student> getStudentById(Long id) {
+        return studentRepository.findById(id);
+    }
 }
