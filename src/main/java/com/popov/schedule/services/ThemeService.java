2c363d8f21726e87060de2f8218b31db4e7c82f7
anton <popovwork@gmail.com>
2022-01-25 10:01:53 +0200

    implemented ThemeService

--- src/main/java/com/popov/schedule/services/ThemeService.java
+++ src/main/java/com/popov/schedule/services/ThemeService.java
@@ -1,7 +1,15 @@
 package com.popov.schedule.services;
 
+import com.popov.schedule.models.Theme;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public interface ThemeService {
+    boolean saveTheme(Theme theme);
+
+    boolean updateTheme(String name, Long teacherId, Long id);
+
+    Optional<Theme> getThemeById(Long id);
 }
--- src/main/java/com/popov/schedule/services/ThemeServiceImpl.java
+++ src/main/java/com/popov/schedule/services/ThemeServiceImpl.java
@@ -1,7 +1,47 @@
 package com.popov.schedule.services;
 
+import com.popov.schedule.models.Theme;
+import com.popov.schedule.repositories.ThemeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ThemeServiceImpl implements ThemeService {
+
+    public final ThemeRepository themeRepository;
+
+    public ThemeServiceImpl(ThemeRepository themeRepository) {
+        this.themeRepository = themeRepository;
+    }
+
+    @Override
+    public boolean saveTheme(Theme theme) {
+        Optional<Theme> themeOptional = themeRepository.getThemeByName(theme.getName());
+
+        if (themeOptional.isPresent()) {
+            return false;
+        }
+
+        themeRepository.save(theme);
+
+        return true;
+    }
+
+    @Override
+    public boolean updateTheme(String name, Long teacherId, Long id) {
+        Optional<Theme> themeOptional = themeRepository.findById(id);
+        if (themeOptional.isEmpty()) {
+            return false;
+        }
+
+        themeRepository.updateTheme(name, teacherId, id);
+
+        return true;
+    }
+
+    @Override
+    public Optional<Theme> getThemeById(Long id) {
+        return themeRepository.findById(id);
+    }
 }
