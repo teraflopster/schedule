5fc6fc46cbac3474e6b4a9c4c945ccac96a00a06
anton <popovwork@gmail.com>
2022-01-25 09:55:06 +0200

    implemented Department model

--- src/main/java/com/popov/schedule/models/Department.java
+++ src/main/java/com/popov/schedule/models/Department.java
@@ -1,4 +1,33 @@
 package com.popov.schedule.models;
 
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "department")
 public class Department {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "faculty_id")
+    private long facultyId;
+
+    public Department() {}
+
+    public Department(String name, long faculty_id) {
+        this.name = name;
+        this.facultyId = faculty_id;
+    }
 }
