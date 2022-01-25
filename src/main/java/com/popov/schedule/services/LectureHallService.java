17f0811cc1b2be4902c76794344b23310b7bd1f7
anton <popovwork@gmail.com>
2022-01-25 09:57:33 +0200

    implemented LectureHall service

--- src/main/java/com/popov/schedule/services/LectureHallService.java
+++ src/main/java/com/popov/schedule/services/LectureHallService.java
@@ -1,7 +1,11 @@
 package com.popov.schedule.services;
 
+import com.popov.schedule.models.LectureHall;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface LectureHallService {
+    void saveLectureHall(LectureHall lectureHall);
+
+    LectureHall getLectureHallById(Long id);
 }
--- src/main/java/com/popov/schedule/services/LectureHallServiceImpl.java
+++ src/main/java/com/popov/schedule/services/LectureHallServiceImpl.java
@@ -1,7 +1,25 @@
 package com.popov.schedule.services;
 
+import com.popov.schedule.models.LectureHall;
+import com.popov.schedule.repositories.LectureHallRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LectureHallServiceImpl implements LectureHallService {
+
+    private final LectureHallRepository lectureHallRepository;
+
+    public LectureHallServiceImpl(LectureHallRepository lectureHallRepository) {
+        this.lectureHallRepository = lectureHallRepository;
+    }
+
+    @Override
+    public void saveLectureHall(LectureHall lectureHall) {
+        lectureHallRepository.save(lectureHall);
+    }
+
+    @Override
+    public LectureHall getLectureHallById(Long id) {
+        return lectureHallRepository.getById(id);
+    }
 }
