2926c2b65d2e6dbaf90ec46cb83d12756d5f3645
anton <popovwork@gmail.com>
2022-01-25 10:00:54 +0200

    created Teacher model

--- src/main/java/com/popov/schedule/models/Teacher.java
+++ src/main/java/com/popov/schedule/models/Teacher.java
@@ -1,4 +1,47 @@
 package com.popov.schedule.models;
 
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Data
+@Entity
+@Table(name = "teacher")
 public class Teacher {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "department_id")
+    private Long departmentId;
+
+    public Teacher() {}
+
+    public Teacher(String name, Long departmentId) {
+        this.name = name;
+        this.departmentId = departmentId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Teacher teacher = (Teacher) o;
+        return Objects.equals(name, teacher.name) && Objects.equals(departmentId, teacher.departmentId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, departmentId);
+    }
 }
