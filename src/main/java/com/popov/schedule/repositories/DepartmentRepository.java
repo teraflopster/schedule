0ac8b2cf79c5696bd645e6d52dcdb3a82fc7069c
anton <popovwork@gmail.com>
2022-01-25 09:55:23 +0200

    created Department repository

--- src/main/java/com/popov/schedule/repositories/DepartmentRepository.java
+++ src/main/java/com/popov/schedule/repositories/DepartmentRepository.java
@@ -5,6 +5,11 @@ import com.sun.xml.bind.v2.model.core.ID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface DepartmentRepository extends JpaRepository<Department, ID> {
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    Optional<Department> getDepartmentById(Long id);
+    List<Department> getDepartmentByFacultyId(Long id);
 }
