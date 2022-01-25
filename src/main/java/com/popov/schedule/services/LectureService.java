e98040c5bbf9c444af9130012d30f3fd557f5a48
anton <popovwork@gmail.com>
2022-01-25 09:58:06 +0200

    implemented Lecture service

--- src/main/java/com/popov/schedule/services/LectureService.java
+++ src/main/java/com/popov/schedule/services/LectureService.java
@@ -1,7 +1,14 @@
 package com.popov.schedule.services;
 
+import com.popov.schedule.models.Day;
+import com.popov.schedule.models.Lecture;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface LectureService {
+    void saveLecture(Lecture lecture);
+
+    List<Lecture> getLectureByDay(Long id);
 }
--- src/main/java/com/popov/schedule/services/LectureServiceImpl.java
+++ src/main/java/com/popov/schedule/services/LectureServiceImpl.java
@@ -1,7 +1,28 @@
 package com.popov.schedule.services;
 
+import com.popov.schedule.models.Day;
+import com.popov.schedule.models.Lecture;
+import com.popov.schedule.repositories.LectureRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LectureServiceImpl implements LectureService {
+
+    private final LectureRepository lectureRepository;
+
+    public LectureServiceImpl(LectureRepository lectureRepository) {
+        this.lectureRepository = lectureRepository;
+    }
+
+    @Override
+    public void saveLecture(Lecture lecture) {
+        lectureRepository.save(lecture);
+    }
+
+    @Override
+    public List<Lecture> getLectureByDay(Long id) {
+        return lectureRepository.getLectureByDay(id);
+    }
 }
