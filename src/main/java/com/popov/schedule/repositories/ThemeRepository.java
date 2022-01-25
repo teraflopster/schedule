9a28bcda2412c746aa39d7ae4ec94f7836085e23
anton <popovwork@gmail.com>
2022-01-25 10:01:38 +0200

    created Theme repository

--- src/main/java/com/popov/schedule/repositories/ThemeRepository.java
+++ src/main/java/com/popov/schedule/repositories/ThemeRepository.java
@@ -1,10 +1,20 @@
 package com.popov.schedule.repositories;
 
 import com.popov.schedule.models.Theme;
-import com.sun.xml.bind.v2.model.core.ID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+import java.util.Optional;
+
 @Repository
-public interface ThemeRepository extends JpaRepository<Theme, ID> {
+public interface ThemeRepository extends JpaRepository<Theme, Long> {
+    Optional<Theme> getThemeByName(String name);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Theme theme SET theme.name = ?1, theme.teacherId = ?2 where theme.id = ?3")
+    void updateTheme(String name, Long teacherId, Long id);
 }
