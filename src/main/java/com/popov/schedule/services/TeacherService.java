b47043520c8a2e1fe2b48d04638047981526f062
anton <popovwork@gmail.com>
2022-01-25 10:01:09 +0200

    implemented Teacher service

--- src/main/java/com/popov/schedule/services/TeacherService.java
+++ src/main/java/com/popov/schedule/services/TeacherService.java
@@ -1,7 +1,19 @@
 package com.popov.schedule.services;
 
+import com.popov.schedule.models.Teacher;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public interface TeacherService {
+    boolean saveTeacher(Teacher teacher);
+
+    Optional<Teacher> getTeacherById(Long id);
+
+    Optional<Teacher> getTeacherByName(String name);
+
+    boolean updateTeacher(String name, Long departmentId, Long id);
+
+    boolean deleteTeacher(Long id);
 }
--- src/main/java/com/popov/schedule/services/TeacherServiceImpl.java
+++ src/main/java/com/popov/schedule/services/TeacherServiceImpl.java
@@ -1,7 +1,67 @@
 package com.popov.schedule.services;
 
+import com.popov.schedule.models.Teacher;
+import com.popov.schedule.repositories.TeacherRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class TeacherServiceImpl implements TeacherService {
+
+    private final TeacherRepository teacherRepository;
+
+    public TeacherServiceImpl(TeacherRepository teacherRepository) {
+        this.teacherRepository = teacherRepository;
+    }
+
+
+    @Override
+    public boolean saveTeacher(Teacher teacher) {
+        Optional<Teacher> teacherOptional = teacherRepository.getTeacherByName(teacher.getName());
+
+        if (teacherOptional.isPresent()) {
+            return false;
+        }
+
+        teacherRepository.save(teacher);
+
+        return true;
+    }
+
+    @Override
+    public Optional<Teacher> getTeacherById(Long id) {
+        return teacherRepository.getTeacherById(id);
+    }
+
+    @Override
+    public Optional<Teacher> getTeacherByName(String name) {
+        return teacherRepository.getTeacherByName(name);
+    }
+
+    @Override
+    public boolean updateTeacher(String name, Long departmentId, Long id) {
+        Optional<Teacher> teacherOptional = teacherRepository.getTeacherById(id);
+
+        if (teacherOptional.isEmpty()) {
+            return false;
+        }
+
+        teacherRepository.updateTeacher(name, departmentId, id);
+
+        return true;
+    }
+
+    @Override
+    public boolean deleteTeacher(Long id) {
+        Optional<Teacher> teacherOptional = teacherRepository.getTeacherById(id);
+
+        if (teacherOptional.isEmpty()) {
+            return false;
+        }
+
+        teacherRepository.deleteTeacherById(id);
+
+        return true;
+    }
 }
