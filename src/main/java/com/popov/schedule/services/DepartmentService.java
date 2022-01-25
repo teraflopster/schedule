541d88cbfdc63d17d5a60c8cce6d537731fb8c16
anton <popovwork@gmail.com>
2022-01-25 09:55:55 +0200

    implemented department service

--- src/main/java/com/popov/schedule/services/DepartmentService.java
+++ src/main/java/com/popov/schedule/services/DepartmentService.java
@@ -1,7 +1,18 @@
 package com.popov.schedule.services;
 
+import com.popov.schedule.models.Department;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public interface DepartmentService {
+    void saveDepartment(Department department);
+
+    Optional<Department> getDepartmentById(Long id);
+
+    void deleteDepartmentById(Long id);
+
+    List<Department> getAllDepartmentsByFaculty(Long id);
 }
--- src/main/java/com/popov/schedule/services/DepartmentServiceImpl.java
+++ src/main/java/com/popov/schedule/services/DepartmentServiceImpl.java
@@ -1,7 +1,38 @@
 package com.popov.schedule.services;
 
+import com.popov.schedule.models.Department;
+import com.popov.schedule.repositories.DepartmentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService {
+
+    private final DepartmentRepository departmentRepository;
+
+    public DepartmentServiceImpl(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
+
+    @Override
+    public void saveDepartment(Department department) {
+        departmentRepository.save(department);
+    }
+
+    @Override
+    public Optional<Department> getDepartmentById(Long id) {
+        return departmentRepository.getDepartmentById(id);
+    }
+
+    @Override
+    public void deleteDepartmentById(Long id) {
+        departmentRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Department> getAllDepartmentsByFaculty(Long id) {
+        return departmentRepository.getDepartmentByFacultyId(id);
+    }
 }
