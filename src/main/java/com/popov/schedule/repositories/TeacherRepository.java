eac45f74aa41fcb88e1da89e4dfbf05a3a40160d
anton <popovwork@gmail.com>
2022-01-25 10:01:19 +0200

    created Teacher repository

--- src/main/java/com/popov/schedule/repositories/TeacherRepository.java
+++ src/main/java/com/popov/schedule/repositories/TeacherRepository.java
@@ -1,10 +1,27 @@
 package com.popov.schedule.repositories;
 
 import com.popov.schedule.models.Teacher;
-import com.sun.xml.bind.v2.model.core.ID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+import java.util.Optional;
+
 @Repository
-public interface TeacherRepository extends JpaRepository<Teacher, ID> {
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+
+    @Transactional
+    void deleteTeacherById(Long id);
+
+    Optional<Teacher> getTeacherByName(String name);
+
+    Optional<Teacher> getTeacherById(Long id);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Teacher teacher SET teacher.name = ?1, teacher.departmentId = ?2 where teacher.id = ?3")
+    void updateTeacher(String name, Long departmentId, Long id);
+
 }
